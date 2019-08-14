f4d498c7120a041dc8ed6074ab225d0de3fdddee
christian <christiancastillejo@gmail.com>
2019-08-14 21:36:31 +0200

    add router and css. add menu home and articles.

---
+++ src/components/menu.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Menu() {
+  return <div className="menu-container">menu</div>;
+}
+
+export default Menu;
---
+++ src/screens/home.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Home() {
+  return <div className="a">home </div>;
+}
+
+export default Home;
