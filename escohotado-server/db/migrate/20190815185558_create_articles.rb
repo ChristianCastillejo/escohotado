b417cf0d3b835449a34450e33351ce3fd59c1115
christian <christiancastillejo@gmail.com>
2019-08-15 21:33:45 +0200

    add article backend

---
+++ escohotado-server/db/migrate/20190815185558_create_articles.rb
@@ -0,0 +1,14 @@
+class CreateArticles < ActiveRecord::Migration[5.2]
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.string :description
+      t.string :images
+      t.string :body
+      t.string :date
+      t.string :tags
+
+      t.timestamps
+    end
+  end
+end
