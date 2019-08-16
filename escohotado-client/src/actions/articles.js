44a52e42c284acd11030c589a39c60ab61f7d91b
christian <christiancastillejo@gmail.com>
2019-08-16 02:24:20 +0200

    add articles

--- escohotado-client/src/actions/articles.js
+++ escohotado-client/src/actions/articles.js
@@ -1,11 +1,12 @@
 import { FETCH_ARTICLES } from "./actionTypes";
 
 export async function fetchArticles() {
-  const response = await fetch("http://localhost:4000/articles/",{
-    method: 'GET',
+  const response = await fetch("http://localhost:4000/articles/", {
+    method: "GET",
     headers: {
       "Content-Type": "application/json"
-    }})
+    }
+  });
   const data = await response.json();
 
   return {
--- escohotado-client/src/screens/articles.js
+++ escohotado-client/src/screens/articles.js
@@ -1,16 +1,107 @@
-import React, { useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { fetchArticles } from "../actions/articles";
 
 function Articles() {
   const articlesStore = useSelector(state => state.article);
   const dispatch = useDispatch();
+  const [openFilter, setOpenFilter] = useState(false);
+  const [filters, setFilters] = useState({
+    philosophy: false,
+    comunism: false,
+    drugs: false,
+    ecomomy: false,
+    history: false,
+    general: false
+  });
 
-  useEffect(() => {
-    dispatch(fetchArticles());
-  }, [dispatch]);
-
-  return <div className="screen">Artículos </div>;
+  useEffect(
+    () => {
+      dispatch(fetchArticles());
+    },
+    [dispatch]
+  );
+  function updateFilters(field, value) {
+    let newFilters = { ...filters };
+    newFilters[field] = value;
+    setFilters(newFilters);
+  }
+  return (
+    <div className="screen articles-container">
+      <div className="articles-articles">
+        <div className="articles-header">
+          <h1>Artículos</h1>
+          <div
+            className="articles-filter-button"
+            onClick={() => setOpenFilter(!openFilter)}
+          >
+            <i className="fa fa-sliders" />
+          </div>
+        </div>
+        <div
+          className={`articles-filter articles-filter${!openFilter &&
+            "--close"}`}
+        >
+          <div
+            onClick={() => updateFilters("philosophy", !filters.philosophy)}
+            className={`articles-filter-item articles-filter-item${filters.philosophy &&
+              "--selected"}`}
+          >
+            Filosofía
+          </div>
+          <div
+            onClick={() => updateFilters("comunism", !filters.comunism)}
+            className={`articles-filter-item articles-filter-item${filters.comunism &&
+              "--selected"}`}
+          >
+            Comunismo
+          </div>
+          <div
+            onClick={() => updateFilters("drugs", !filters.drugs)}
+            className={`articles-filter-item articles-filter-item${filters.drugs &&
+              "--selected"}`}
+          >
+            Drogas
+          </div>
+          <div
+            onClick={() => updateFilters("history", !filters.history)}
+            className={`articles-filter-item articles-filter-item${filters.history &&
+              "--selected"}`}
+          >
+            Historia
+          </div>
+          <div
+            onClick={() => updateFilters("general", !filters.general)}
+            className={`articles-filter-item articles-filter-item${filters.general &&
+              "--selected"}`}
+          >
+            General
+          </div>
+        </div>
+        {articlesStore.map(article => (
+          <div key={article.id} className="articles-article">
+            <img
+              alt="article"
+              className="articles-article-img"
+              src={article.images}
+            />
+            <div className="articles-articles-details">
+              <h3 className="articles-article-title">{article.title}</h3>
+              <p className="articles-article-body">
+                {article.body && article.body.slice(0, 500).concat("... ")}
+                <span>Segir Leyendo</span>
+              </p>
+              {article.tags.split(",").map(tag => (
+                <span key={tag} className="articles-article-tags">
+                  {tag}
+                </span>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 
 export default Articles;
