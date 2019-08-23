ad59d2de5c2b766b76ebbefa7825f497bb88a29c
christian <christiancastillejo@gmail.com>
2019-08-23 14:30:10 +0200

    edit home

--- escohotado-client/src/screens/home.js
+++ escohotado-client/src/screens/home.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 function Home() {
-  return <div className="screen home-container">Por favor, pulsa en la sección "Artíclulos". </div>;
+  return <div className="screen home-container">Por favor, pulsa en la sección "Artículos". </div>;
 }
 
 export default Home;
