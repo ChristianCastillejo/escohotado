4c75b0e19b316c9084050fd056b17b1185a0d5ab
Christian Castillejo <christiancastillejo@gmail.com>
2019-08-25 16:47:37 +0200

    update articles UI, case insensitive search

--- escohotado-client/src/screens/articles.js
+++ escohotado-client/src/screens/articles.js
@@ -18,7 +18,7 @@ const categories = {
 function Articles(props) {
   const articles = useSelector(state => state.articles);
   const dispatch = useDispatch();
-  const [openFilter, setOpenFilter] = useState(false);
+  const [openSearch, setopenSearch] = useState(false);
   const [search, setSearch] = useState("");
   const [text, setText] = useState("");
   const [filters, setFilters] = useState({
@@ -87,76 +87,76 @@ function Articles(props) {
       ) : (
         <div className="articles-container">
           <div className="articles-articles">
-            <div className="articles-header">
-              <h1>Artículos</h1>
-              {Object.values(filters).some(e => e === true) || text !== "" ? (
-                <p className="articles-header-p">Filtros activados</p>
-              ) : (
-                <p className="articles-header-p articles-header-p--hidden" />
-              )}
+            <div
+              className={`articles-filter-categories articles-filter-categories${openSearch &&
+                "--search"}`}
+            >
               <div
-                className="articles-filter-button"
-                onClick={() => setOpenFilter(!openFilter)}
+              className={`articles-filter-button articles-filter-button${openSearch &&
+                "--search"}`}
+                onClick={() => setopenSearch(!openSearch)}
               >
                 <i className="fas fa-search" />
               </div>
+              <input
+                className={`articles-filter-search-input articles-filter-search-input${!openSearch &&
+                  "--hidden"}`}
+                placeholder="De la piel para dentro..."
+                onChange={e => setSearch(e.target.value)}
+                onKeyPress={e => {
+                  if (e.key === 'Enter') {
+                    updateFilters(undefined, undefined, search)
+                  }
+                }}
+              />
+              <button
+              className={`articles-filter-search-button articles-filter-search-button${!openSearch &&
+                "--hidden"}`}
+                onClick={() => updateFilters(undefined, undefined, search)}
+              >
+                <p>Buscar</p>
+              </button>
+              <div
+                onClick={() =>
+                  updateFilters("philosophy", !filters.philosophy, search)
+                }
+                className={`articles-filter-item ${filters.philosophy &&
+                  "articles-filter-item--selected"} ${openSearch &&
+                    "articles-filter-item--hidden"}`}
+              >
+                Filosofía
+              </div>
+              <div
+                onClick={() =>
+                  updateFilters("comunism", !filters.comunism, search)
+                }
+                className={`articles-filter-item ${filters.comunism &&
+                  "articles-filter-item--selected"} ${openSearch &&
+                    "articles-filter-item--hidden"}`}
+              >
+                Comunismo
+              </div>
+              <div
+                onClick={() => updateFilters("drugs", !filters.drugs, search)}
+                className={`articles-filter-item ${filters.drugs &&
+                  "articles-filter-item--selected"} ${openSearch &&
+                    "articles-filter-item--hidden"}`}
+              >
+                Drogas
+              </div>
+              <div
+                onClick={() =>
+                  updateFilters("history", !filters.history, search)
+                }
+                className={`articles-filter-item ${filters.history &&
+                  "articles-filter-item--selected"} ${openSearch &&
+                    "articles-filter-item--hidden"}`}
+              >
+                Historia
+              </div>
             </div>
-            <div
-              className={`articles-filter articles-filter${!openFilter &&
-                "--close"}`}
-            >
-              <div className="articles-filter-categories">
-                <div
-                  onClick={() =>
-                    updateFilters("philosophy", !filters.philosophy, search)
-                  }
-                  className={`articles-filter-item articles-filter-item${filters.philosophy &&
-                    "--selected"}`}
-                >
-                  Filosofía
-                </div>
-                <div
-                  onClick={() =>
-                    updateFilters("comunism", !filters.comunism, search)
-                  }
-                  className={`articles-filter-item articles-filter-item${filters.comunism &&
-                    "--selected"}`}
-                >
-                  Comunismo
-                </div>
-                <div
-                  onClick={() => updateFilters("drugs", !filters.drugs, search)}
-                  className={`articles-filter-item articles-filter-item${filters.drugs &&
-                    "--selected"}`}
-                >
-                  Drogas
-                </div>
-                <div
-                  onClick={() =>
-                    updateFilters("history", !filters.history, search)
-                  }
-                  className={`articles-filter-item articles-filter-item${filters.history &&
-                    "--selected"}`}
-                >
-                  Historia
-                </div>
-              </div>
-              <div className="articles-filter-search">
-                <div className="articles-filter-search-title">
-                  Buscar por título o contenido:
-                </div>
-                <input
-                  className="articles-filter-search-input"
-                  placeholder="De la piel para dentro..."
-                  onChange={e => setSearch(e.target.value.toLowerCase())}
-                />
-                <button
-                  className="articles-filter-search-button"
-                  onClick={() => updateFilters(undefined, undefined, search)}
-                >
-                  <p>Buscar</p>
-                </button>
-              </div>
+            <div className="articles-header">
+              <h1>Artículos</h1>
             </div>
             {articles.length === 0 ? (
               <p className="articles-error-message">
