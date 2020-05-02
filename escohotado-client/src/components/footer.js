90ca58222d423908121613abb0650315036ce52e
christian <christiancastillejo@gmail.com>
2020-05-02 17:12:14 +0200

    removed fade from footer

--- escohotado-client/src/components/footer.js
+++ escohotado-client/src/components/footer.js
@@ -1,50 +1,43 @@
 import React from "react";
-import Fade from "react-reveal/Fade";
 
 function Menu() {
   return (
-    <Fade right cascade>
-      <div className="footer">
-        <p>Antonio Escohotado 2020.</p>
-        <div className="social-network">
-          <a
-            href="https://www.facebook.com/AEscohotado/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <i className="fab fa-facebook" />
-          </a>
-          <a
-            href="https://twitter.com/AEscohotado"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <i className="fab fa-twitter" />
-          </a>
-          <a
-            href="https://www.instagram.com/escohotado/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <i className="fab fa-instagram" />
-          </a>
-          <a
-            href="https://www.youtube.com/channel/UCks2FdxaBZZFl4PTBAGz4Jw"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <i className="fab fa-youtube" />
-          </a>
-        </div>
+    <div className="footer">
+      <p>Antonio Escohotado 2020.</p>
+      <div className="social-network">
         <a
-          href="https://ezsoftware.io"
+          href="https://www.facebook.com/AEscohotado/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          <p>Creado por Christian Castillejo</p>
+          <i className="fab fa-facebook" />
+        </a>
+        <a
+          href="https://twitter.com/AEscohotado"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fab fa-twitter" />
+        </a>
+        <a
+          href="https://www.instagram.com/escohotado/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fab fa-instagram" />
+        </a>
+        <a
+          href="https://www.youtube.com/channel/UCks2FdxaBZZFl4PTBAGz4Jw"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <i className="fab fa-youtube" />
         </a>
       </div>
-    </Fade>
+      <a href="https://ezsoftware.io" target="_blank" rel="noopener noreferrer">
+        <p>Creado por Christian Castillejo</p>
+      </a>
+    </div>
   );
 }
 
